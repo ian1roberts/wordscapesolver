204228e22419125bf39c00f4719c0dbd0029969a
Ian Roberts <ian.roberts@cantab.net>
2022-11-23 19:37:51 +0000

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 config = {
-    "version": "0.3.0",
+    "version": "0.4.0",
     "name": "wordscapesolver",
     "description": "<MODULE_DESCRIPTION>",
     "url": "",
